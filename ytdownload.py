b6d7461f322fdcdf8fd5065e612405998d972432
ZVANGI_UwU <59018467+ZVANGI-here@users.noreply.github.com>
2020-12-15 07:08:39 +0100

    Add files via upload

---
+++ ytdownload.py
@@ -0,0 +1,31 @@
+#libraries
+
+from pytube import YouTube 
+
+#url
+
+url=input("Enter the YouTube Video Link ! : ")
+
+#wait
+
+print("")
+print("")
+print("")
+print("Please wait .... ")
+
+#script_by
+
+yt=YouTube(url)
+print("ZVANGI's script")
+
+#download
+
+print(yt.title)
+yt.streams.first().download()
+
+#Done
+
+print("")
+print("Download Done !")
+
+#end
