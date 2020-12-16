db1d3a74bbd4ad8c83e6bae295293e5a2bb669e1
ZVANGI_UwU <59018467+ZVANGI-here@users.noreply.github.com>
2020-12-16 09:03:58 +0100

    Add files via upload
    more detailed & can chose quality & if "mp4"or "mp3"
    
    ((still working in it this's a BETA version))

---
+++ ytdownload ---V2---.py
@@ -0,0 +1,33 @@
+#libraries 
+#|------------|---------|
+
+from pytube import YouTube
+
+#variabels
+#|------------|---------|
+
+url = input('Enter The Video Link here : ')
+yt = YouTube(url)
+vids = yt.streams.all()
+
+
+#quality_download   
+#|------------|---------|
+
+i = 1
+for stream in vids:
+    print (str(i) + " - " + str(stream))
+    i += 1
+
+stream_quality = int(input('Enter The quality number you want : '))
+
+#place_download & downloading
+
+video = vids[stream_quality - 1]
+print ('The Download place is in [[C:\Users\ZVANGI\Downloads]]')
+video.download("C:\Users\ZVANGI\Downloads")
+
+print ('here we go sir . . . Your download has been completed')
+print ('script by --ZVANGI--     OwO')
+
+#Done !!
